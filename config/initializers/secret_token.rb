0661da248df48820058284fe3e3d263b6b7b9d7c
alexis finch <agent.fin@gmail.com>
2012-07-27 20:53:13 -0700

    WE DO THE COMMITING AGAIN AND WE LIKE IT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwantApp::Application.config.secret_token = '2d3ab6d006608f02bac558c7166b44a375c93b72b9db93d6c1dad305fb7bff5ed18855cf77c2ed232bae7568427ebada3c2d16e18d6aab268da08794b2b1795f'
