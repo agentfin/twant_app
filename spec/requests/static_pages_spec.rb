c0080655bb4a6193e03654c5106522d1ccf7fed4
alexis finch <agent.fin@gmail.com>
2012-07-28 16:08:08 -0700

    Added pants to Home, asked for HALP! and created About Us

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,11 +4,45 @@ describe "Static pages" do
   
   describe "Home page" do
   
-    it "works! Oh, and it shoud lhave the content 'TwantApp'" do
-      # Run the generator again with the --webrat flag 
-      # if you want to use webrat methods/matchers
-      visit '/static_pages_index/home'
-      page.should have_content( 'TwantApp' )
-      end
+    it "should have the h1 'TwantApp'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'TwantApp' )
+    end
+  
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector( 'title', :text => "TwantApp | Home")
+    end
+  
   end
+  
+
+  describe "Help page" do
+    
+    it "should have the h1 'HALP!" do
+      visit '/static_pages/help'
+      page.should have_selector( 'h1', :text => 'HALP!' )
+    end
+    
+    it "should have the title 'HALP!'" do
+      visit '/static_pages/help'
+      page.should have_selector( 'title', :text => "TwantApp | HALP!")
+    end
+  
+  end
+  
+  describe "About page" do
+    
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    
+    it "should have the title 'About Us" do
+      visit '/static_pages/about'
+      page.should have_selector( 'title', :text => "TwantApp | About Us")
+    end
+    
+  end
+
 end
